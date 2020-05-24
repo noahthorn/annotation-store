15133a8fc7bf625dd204083c7bbf84963ffaeacb
noahthorn <noahthornburgh@gmail.com>
2020-05-24 13:39:36 -0500

    -aligned items by position and z-index -added pop-up cards for annotations and helpbutton

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -4,8 +4,36 @@ import { computed, action } from '@ember/object'
 export default class BooksController extends Controller {
 	isOpen = true
 
+	prevBox = null
+
 	@action
 	toggle() {
 		this.toggleProperty('isOpen');
+	}
+
+	@action
+	helpButton() {
+		let helpPop = document.body.querySelector('#help-pop');
+		helpPop.classList.toggle('hide');
+	}
+
+	@action
+	selectPassage(passage_id) {
+		if (this.prevBox != null) {
+			this.prevBox.classList.toggle('hide');
+		}
+
+		// display box logic
+		let pairOutlet = document.body.querySelector("#outlet-" + passage_id);
+		console.log(pairOutlet);
+		pairOutlet.previousElementSibling.classList.add('relative');
+		pairOutlet.classList.toggle('hide');
+		// pairOutlet.classList.add('annotation-box');
+
+		// set this outlet to global
+		this.set('prevBox', pairOutlet);
+
+		// move to annotations route
+		this.transitionToRoute('books.passages.annotations', passage_id);
 	}
 }
--- app/routes/books/passages/annotations.js
+++ app/routes/books/passages/annotations.js
@@ -6,4 +6,12 @@ export default class BooksPassagesAnnotationsRoute extends Route {
 		let passage = await this.store.findRecord('passage', params.passage_id, { include: 'annotations' });
 		return passage;
 	}
+
+	// Use books controller for hover box
+	renderTemplate(controller, model) {
+		let booksController = this.controllerFor('books');
+		this.render('books.passages.annotations', {
+			controller: booksController
+		});
+	}
 }
